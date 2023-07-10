537b55addaa998c662c5daf4cc0be73746b21137
Jonas Wittstrøm <jonaswittstrom1@gmail.com>
2023-07-10 16:17:28 +0200

    Car now has different states visualized by color from spriterenderer.

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 _hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 _noPackageColor = new Color32(0, 0, 0, 0);
+
     [SerializeField] float _destroyDelay = 1.0f;
     bool _hasPackage;
+
+    SpriteRenderer _spriteRenderer;
+
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,6 +28,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up!");
             _hasPackage = true;
+            _spriteRenderer.color = _hasPackageColor;
             Destroy(other.gameObject, _destroyDelay);
         }
 
@@ -25,6 +36,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Delivery completed!");
             _hasPackage = false;
+            _spriteRenderer.color = _noPackageColor;
             Destroy(other.gameObject, _destroyDelay);
         }            
     }
